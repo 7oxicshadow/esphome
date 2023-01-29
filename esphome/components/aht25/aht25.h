5be9078ce81e6f5e1ed675eb728aa314c6afe88d
7oxicshadow <7oxicshadow@googlemail.com>
2023-01-29 17:23:37 +0000

    fix prototype

--- esphome/components/aht25/aht25.h
+++ esphome/components/aht25/aht25.h
@@ -22,7 +22,7 @@ class AHT25Component : public PollingComponent, public i2c::I2CDevice {
   sensor::Sensor *humidity_sensor_{nullptr};
 
  private:
-  void reset_REG(uint8_t addr) override;
+  uint8_t reset_REG(uint8_t addr) override;
  };
 
 }  // namespace aht25
